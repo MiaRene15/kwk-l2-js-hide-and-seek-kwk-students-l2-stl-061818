d25e214da7d55dab2376b56e67786f1eaeb6b69b
MiaRene15 <kids.hines@icloud.com>
2018-06-21 20:52:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,5 +14,17 @@ function deepestChild() {
 }
 
 function increaseRankBy(n) {
-  return document.querySelectorAll('.ranked-list')
+  rconst rankedLists = document.querySelectorAll('.ranked-list')
+const firstList = rankedLists[0]
+const secondList = rankedLists[1]
+let children = firstList.children
+let start = 1
+for (let i = 0, l = children.length; i < l; i++) {
+  expect(parseInt(children[i].innerHTML)).toEqual(start + i + 3)
+}
+children = secondList.children
+start = 12
+for (let i = 0, l = children.length; i < l; i++) {
+  expect(parseInt(children[i].innerHTML)).toEqual(start - i + 3)
+}
 }
